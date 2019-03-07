1ba7304ab566dd3127e67f51b424316a43cf86c2
Dhara <dharababariya34@gmail.com>
2019-03-07 15:36:42 +0530

    insert user data

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const user = require('./rotues/rotues');
-const db = require('./db/knex');
-const bodyParser = require('body-parser');
-app.use(user);
-app.use(bodyParser);
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(user);
 
 app.listen(port,() => console.log('listen port 8080'));
--- rotues/rotues.js
+++ rotues/rotues.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db/knex');
+const knex = require('knex')({
+    client: 'pg',
+    version: '7.2',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'postgres',
+      database : 'login'
+    }
+  });
+   
 
 router.use(function (req,res, next){
     if("OPTIONS" === req.method){
@@ -12,6 +22,13 @@ router.use(function (req,res, next){
 
 router.post("/login", async function (req,res) 
 {
+
+    if(req.body === undefined) {
+
+        console.log('True true');
+
+    }
+
     console.log(JSON.stringify(req.body));
     const result = await knex("dhara.login").insert({
         name:req.body.name,
@@ -19,6 +36,7 @@ router.post("/login", async function (req,res)
 
     })
     res.send(result);
+    console.log(result);
 })
     
 module.exports = router;
