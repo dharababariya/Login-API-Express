3ad92dd28c60627cd7ba6da77cffee7222bf8514
Dhara <dharababariya34@gmail.com>
2019-03-07 09:35:57 +0530

    create simple api with express

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('hello Dhara');
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/' , (req,res) => res.send('hello i m here!'));
+app.listen(port,() => console.log('listen port 8080'));
